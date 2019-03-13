c2749f6b09da6d372db1d798112cb7378a757484
Ryan Pearce <pearcer15@gmail.com>
2019-03-13 08:54:59 -0400

    started a model

---
+++ WebApplication.Web/Models/Park.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Web.Models
+{
+    /// <summary>
+    /// This represents a park
+    /// </summary>
+    public class Park
+    {
+
+    }
+}

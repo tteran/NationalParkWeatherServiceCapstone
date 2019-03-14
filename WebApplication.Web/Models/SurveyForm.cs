93fcf726a5a91bbd736f00006dbdf600905f90ed
Ryan Pearce <pearcer15@gmail.com>
2019-03-14 09:02:16 -0400

    Survey Form Model

---
+++ WebApplication.Web/Models/SurveyForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Web.Models
+{
+    public class SurveyForm
+    {
+        /// <summary>
+        /// The code of the park voted for
+        /// </summary>
+        public string ParkCode { get; set; }
+
+        /// <summary>
+        /// The email address of the voter
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The home state of the Voter
+        /// </summary>
+        public string State { get; set; }
+
+        /// <summary>
+        /// The Voter's activity level
+        /// </summary>
+        public string ActivityLevel { get; set; }
+    }
+}

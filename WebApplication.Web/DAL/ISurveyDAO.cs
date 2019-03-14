5cea7d3270d7f22dc4aeea2ebaa63d1f0d39e025
Ryan Pearce <pearcer15@gmail.com>
2019-03-14 09:17:06 -0400

    fixed SQL

---
+++ WebApplication.Web/DAL/ISurveyDAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Web.Models;
+
+namespace WebApplication.Web.DAL
+{
+    public interface ISurveyDAO
+    {
+        bool SaveSurvey(SurveyForm survey);
+
+        IList<SurveyForm> GetSurveyResults();
+    }
+}

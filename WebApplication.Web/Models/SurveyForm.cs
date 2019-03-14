ec77875e59ff2aff202ba8fa5765d404d0d8d730
Tyler Teran <teran.tyler@yahoo.com>
2019-03-14 10:13:01 -0400

    Added the form to the view and added data validation to survey model.

--- WebApplication.Web/Models/SurveyForm.cs
+++ WebApplication.Web/Models/SurveyForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,21 +11,29 @@ namespace WebApplication.Web.Models
         /// <summary>
         /// The code of the park voted for
         /// </summary>
+        [Display(Name = "CHOOSE A PARK CODE")]
+        [Required]
         public string ParkCode { get; set; }
 
         /// <summary>
         /// The email address of the voter
         /// </summary>
+        [Display(Name = "EMAIL")]
+        [Required]
         public string Email { get; set; }
 
         /// <summary>
         /// The home state of the Voter
         /// </summary>
+        [Display(Name = "STATE")]
+        [Required]
         public string State { get; set; }
 
         /// <summary>
         /// The Voter's activity level
         /// </summary>
+        [Display(Name = "ACTIVITY LEVEL")]
+        [Required]
         public string ActivityLevel { get; set; }
 
         /// <summary>
